cdaebfdfb20da49318df6ef23e0ed7da131e3eda
ejobe1 <ed.jobe@yahoo.com>
2022-01-14 09:51:50 -0500

    data-structures

--- Queue/queue.py
+++ Queue/queue.py
@@ -1,62 +1,49 @@
-# pop() removes last item within the list while 
-# peek() doesn't remove the item just returns it
-# append() pushes, adds an item within the list
-# LIFO - Last In First Out
-# Stack Memory: data type that stores active functions and local variables
-# Pros: fast access/small size/no fragmentation
-# Heap Memory: the size of the memory is way larger than stack (can store more items), can store objects
-# Pros: large size/slow access/stores obj/may be fragmented(unused memory)
+# FIFO - first item in is the first item removed
+# useful for CPU scheduling such as determining when a process should execute or be on hold
 
-
-class Stack:
+class Queue:
     
     def __init__(self):
-        # empty 1D array
-        self.stack = []
-    
-    # O(1) - adds an item to the list
-    def push(self,data):
-        # inserting an item within the list
-        self.stack.append(data)
-    
-    # O(1) - removes the last item within a list
-    def pop(self):
         
-        if self.stackSize() < 1:
-            return -1
+        self.queue = []
+    
+    # O(1) - boolean, sees if the queue is empty
+    def is_empty(self):
         
-        # getting the last item
-        data = self.stack[-1]
-        # removing the last item
-        del self.stack[-1]
+        return self.queue == []
+    
+    # O(1) - adds data at the end of the 1D array | [1],[2],[3] ...
+    def enqueue(self,data):
+        
+        return self.queue.append(data)
+    
+    # O(n) - removes items at the beginning of the queue and shifts the new items to the left
+    def dequeue(self):
+        
+        # getting rid of the the first item
+        data = self.queue[0]
+        del self.queue[0]
         return data
     
-    # O(1) - returns an item w/o removing it
+    # O(1) - gets the first item on the list without removal
     def peek(self):
+        return self.queue[0]
+
+    # O(1) - just getting array size
+    def queueSize(self):
         
-        if self.stackSize() < 1:
-            return 'This list is empty.'
-        
-        return self.stack[-1]
-    
-    # O(1) - returns True or False if the list is empty
-    def is_empty(self):
-        return self.stack == []
+        return len(self.queue)
 
-    def stackSize(self):
-        return len(self.stack)
+queue = Queue()
 
-stack = Stack()
+queue.enqueue(23)
+queue.enqueue(93)
+queue.enqueue(63)
+queue.enqueue(43)
 
-stack.push(32)
-stack.push(22)
-stack.push(11)
-stack.push(99)
-
-print('The size of the stack is: %d' % stack.stackSize())
-print('\nThe item poped was: %d' % stack.pop())
-print('\nThe item poped was: %d' % stack.pop())
-print('\nThe item poped was: %d' % stack.pop())
-print('\nThe new size of the stack is: %d' % stack.stackSize())
-print('\nLast item not removed but shown is: %s' % stack.peek())
-print('\nNo items in stack? %s' % stack.is_empty())
+print('Size before: %s' % queue.queueSize())
+print('\nOld first: %s' % queue.peek())
+print('\nItem removed: %s ' % queue.dequeue())
+print('\nNew first: %s' % queue.peek())
+print('\nIs queue empty? %s' % queue.is_empty())
+print('\nSize after: %s ' % queue.queueSize())
--- Stack/stack.py
+++ Stack/stack.py
@@ -1,62 +1,31 @@
-# pop() removes last item within the list while 
-# peek() doesn't remove the item just returns it
-# append() pushes, adds an item within the list
-# LIFO - Last In First Out
-# Stack Memory: data type that stores active functions and local variables
-# Pros: fast access/small size/no fragmentation
-# Heap Memory: the size of the memory is way larger than stack (can store more items), can store objects
-# Pros: large size/slow access/stores obj/may be fragmented(unused memory)
-
-
 class Stack:
     
     def __init__(self):
-        # empty 1D array
         self.stack = []
     
-    # O(1) - adds an item to the list
-    def push(self,data):
-        # inserting an item within the list
-        self.stack.append(data)
+    def push(self, data):
+        return self.stack.append(data)
     
-    # O(1) - removes the last item within a list
+    def is_empty(self):
+        return self.stack == []
+    
     def pop(self):
-        
-        if self.stackSize() < 1:
-            return -1
-        
-        # getting the last item
         data = self.stack[-1]
-        # removing the last item
         del self.stack[-1]
         return data
     
-    # O(1) - returns an item w/o removing it
     def peek(self):
-        
-        if self.stackSize() < 1:
-            return 'This list is empty.'
-        
         return self.stack[-1]
     
-    # O(1) - returns True or False if the list is empty
-    def is_empty(self):
-        return self.stack == []
-
-    def stackSize(self):
+    def size(self):
         return len(self.stack)
 
-stack = Stack()
+s = Stack()
 
-stack.push(32)
-stack.push(22)
-stack.push(11)
-stack.push(99)
+s.push(12)
+s.push(54)
+s.push(99)
 
-print('The size of the stack is: %d' % stack.stackSize())
-print('\nThe item poped was: %d' % stack.pop())
-print('\nThe item poped was: %d' % stack.pop())
-print('\nThe item poped was: %d' % stack.pop())
-print('\nThe new size of the stack is: %d' % stack.stackSize())
-print('\nLast item not removed but shown is: %s' % stack.peek())
-print('\nNo items in stack? %s' % stack.is_empty())
+print(s.pop())
+print(s.peek())
+    
