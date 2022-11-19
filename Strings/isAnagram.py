224342a1aa2c08c61221871bc92cb244ca1b1203
Edrisa Jobe <ed.jobe@yahoo.com>
2022-11-19 02:14:45 -0500

    Anagram

--- Strings/isAnagram.py
+++ Strings/isAnagram.py
@@ -1,26 +1,34 @@
+# anagram - restful == fluster ('r', 'e', 's', 't', 'f', 'u', 'l')
+# define a func with two params
 
-# word that can be spelt the same ex: care = race, can be spelt either way
-def isAnagram(str1,str2):
+def anagram(str1, str2):
 
-    # we see if the length are same
+    # check the len of whether str1 is equal to str2
     if len(str1) != len(str2):
-
         return False
     
-    # we sort both strings to check ordering
+    # sorting each string value as ['a', 'b', 'c']
     str1 = sorted(str1)
     str2 = sorted(str2)
-
-    # loop and check each index seeing if elements match
+    
+    # for loop to iterate and check the letters with the same indexes
+    # len can be str1 or str2 as they both should equal to the same len
+    # O(n) runtime
     for i in range(len(str1)):
-
-        if str1[i] == str2[i]:
-
-            return True
         
-        return False
-
-print(isAnagram("word", "ordw")) # True
+        # checking to see if they match
+        if str1[i] != str2[i]:
+            return False
     
-    
+    # O(N log N) + O(n) = O(N log N) = linearithmic due to the num of letters within each array
+    return True
 
+if __name__ == '__main__':
+    s1 = ['r', 'e', 's', 't', 'f', 'u', 'l']
+    s2 = ['f', 'l', 'u', 's', 't', 'e', 'r']
+    print(anagram(s1, s2))
+    
+    # or
+    print(anagram('bro', 'rob'))
+        
+        
