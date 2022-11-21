11eb2aa353bf2e181cc152d7afb6c6ec898c9136
Edrisa Jobe <ed.jobe@yahoo.com>
2022-11-21 00:45:25 -0500

    productSelf

--- Array/productExceptSelf.py
+++ Array/productExceptSelf.py
@@ -4,15 +4,21 @@ def ProductExceptSelf(nums):
 
     result = [1] * len(nums) # [1,1,1,1]
 
-    prefix = 1 # [1,2,6,24]
-    postfix = 1 # [24,24,12,4]
+    prefix = 1 # [1] [1,2,3,4]
+    postfix = 1 # [1,2,3,4] [1]
 
-
+    
     for i in range(len(nums)):
+
+        # we take save the elements amd put them in the prefix
+        # we then take those elements and multiply them by the input
         result[i] = prefix
         prefix *= nums[i]
 
+    # we do -1, -1, -1 since we're working backwards from the last digit to the beginning
     for i in range(len(nums) -1, -1, -1):
+
+        # same result as the prefix but doing the inverse to get the backwards output
         result[i] *= postfix
         postfix *= nums[i]
     
