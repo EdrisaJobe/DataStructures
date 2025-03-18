d21c144a9bd235b81448cebd3ffa499b3b860b31
edjob <ed.jobe@yahoo.com>
2025-03-18 00:13:41 -0400

    edit image

--- Array/majorityElement.py
+++ Array/majorityElement.py
@@ -1,27 +1,17 @@
 # An element that appear more than half the array
 # Time: O(n)
+from collections import Counter 
 def majorityElement(nums):
-
-    count = {}
-    res, maxCount = 0, 0
-
-    for n in nums:
-
-        # get the keys' value in n and add 1, default 0
-        count[n] = 1 + count.get(n, 0)
-
-        # we see if the count is bigger than max
-        if count[n] > maxCount:
-
-            # we get the max n 
-            res = n
-
-        # else we keep current result
-        res
-
-        # current count compared to prev max count
-        maxCount = max(count[n], maxCount)
     
-    return res
+    count = Counter(nums)  # Create a Counter object that counts occurrences of each element in nums
+    
+    maxVal = max(count.values())  # Find the maximum frequency among all elements
+                                  # In our example, max of [2,3] is 3
+        
+    for k, v in count.items():  # Iterate through each key-value pair in the Counter
+                                # k is the element, v is its frequency
+            
+        if v == maxVal:  # If the current element's frequency equals the maximum frequency
+            return k   
 
-print(majorityElement([1,1,2,2,2])) # 2
+print(majorityElement([1,1,2,2,2])) # 2  
